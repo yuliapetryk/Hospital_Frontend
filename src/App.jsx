16bf0ab7961e898bed7a0774724cdad01218b384
Yulia Petryk <yulya.petruk.03@gmail.com>
2024-04-17 00:21:57 +0300

    Add main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Auth from './Auth'; 
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn moreeeeeeeeeeeeee
+    <div>
+        <img src="/icon-medicine.png" className="logo" alt=" logo" />
+        <img src="/icon-doctor.png" className="logo doctor" alt="doctor logo" />
+    
+    </div>
+  <h3 class ="subtitle">Приватна поліклініка</h3>
+  <h1 class="title">ProfiMed</h1>
+  <h3 class ="subtitle">Увійти як:</h3>
+  <button class="button">Лікар</button>
+  <button class="button">Пацієнт</button>
+    <div className="card">
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
       </p>
     </div>
+    <div>
+        <Auth />
+      </div>
+  </div>
+    
   )
 }
 
